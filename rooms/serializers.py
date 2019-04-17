8d6c0fd66401b43dceda4f7f5adec6e965a7c310
rammanoj <rammanojpotla1608@gmail.com>
2019-04-17 21:23:28 +0530

    Initial heroku commit

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -34,10 +34,23 @@ class RoomListSerializer(serializers.ModelSerializer):
 
 class UserBookingSerializer(serializers.ModelSerializer):
     rooms = RoomListSerializer(many=True)
+    upload_new_proof = serializers.SerializerMethodField()
+    booked_by = serializers.SerializerMethodField()
+
+    def get_booked_by(self, obj):
+        return obj.booked_by.username
+
+    def get_upload_new_proof(self, obj):
+        user = self.context['request'].user
+        if user == obj.booked_by:
+            return True
+        else:
+            return False
 
     class Meta:
         model = models.Bookings
-        fields = '__all__'
+        fields = ['pk', 'reference', 'rooms', 'start_date', 'end_date', 'booked_by', 'booking_type',
+                  'proof', 'user_booked', 'checkedin', 'upload_new_proof']
 
 
 class RoomSerializer(serializers.ModelSerializer):
@@ -46,100 +59,3 @@ class RoomSerializer(serializers.ModelSerializer):
         model = models.Room
         fields = '__all__'
         read_only_fields = ['pk']
-
-#
-# class RoomBookingSerializer(serializers.Serializer):
-#     from_date = serializers.DateTimeField(required=True)
-#     to_date = serializers.DateTimeField(required=True)
-#     rooms = serializers.ListField(required=False)
-#     capacity=serializers.IntegerField(required=True)
-#     room_type = serializers.CharField(required=False, max_length=10)
-#     id_proof = serializers.FileField(required=True)
-#
-#     def validate_from_date(self, from_date):
-#         if from_date.date() < timezone.now().date():
-#             raise serializers.ValidationError('From date has to be prior to today!!')
-#         return from_date
-#
-#     def validate_to_date(self, to_date):
-#         if to_date.date() < timezone.now().date():
-#             raise serializers.ValidationError('Check out date has to be prior to today')
-#         return to_date
-#
-#     def validate(self, attrs):
-#         if attrs['from_date'] >= attrs['to_date']:
-#             raise serializers.ValidationError('Checkout date has to be prior to checkin date')
-#
-#         start_date = attrs['from_date'].date()
-#         end_date = attrs['to_date'].date()
-#         print(attrs['to_date'])
-#         print(attrs['from_date'])
-#         print(start_date)
-#         print(end_date)
-#
-#         booked_rooms = models.Room.objects.filter(Q(booking__status=0) | Q(booking__status=1)).exclude(Q
-#                 (Q(booking__check_in__gt=start_date),Q(booking__check_in__gt=end_date)) | Q
-#                 (Q(booking__check_out__lt=end_date), Q(booking__check_out__lt=start_date)))
-#
-#         print("Came e")
-#         print(booked_rooms)
-#         rooms = models.Room.objects.all().difference(booked_rooms)
-#
-#         # Check if room to be take are given
-#         try:
-#             room = models.Room.objects.filter(room_no__in=attrs['room']).order_by('-capacity')
-#             if not room.issubset(rooms):
-#                 raise serializers.ValidationError('Rooms you selected are already booked for given dates!!')
-#             else:
-#                 attrs['rooms'] = room
-#                 return attrs
-#         except KeyError:
-#             rooms = rooms.filter(room_type=attrs['room_type']).order_by('-capacity')
-#             pass
-#
-#         total_capacity = sum([i.capacity for i in rooms])
-#         if total_capacity < attrs['capacity']:
-#             raise serializers.ValidationError('There are no rooms avilable for the given dates')
-#
-#         if attrs['capacity'] > 20:
-#             raise serializers.ValidationError('At a max of 20 people can book at an attempt')
-#         sums, j = 0, 0
-#         for i in rooms:
-#             if sums >= attrs['capacity']:
-#                 break
-#             else:
-#                 sums = sums + i.capacity
-#                 j += 1
-#
-#         attrs['rooms'] = rooms[0:j]
-#         return attrs
-#
-#     def create(self, validated_data):
-#         print(validated_data)
-#         try:
-#             username = validated_data['username']
-#         except KeyError:
-#             username = None
-#
-#         reference = str(random.randint(100000000000, 1000000000000))
-#         user = self.context['request'].user
-#         return models.Booking.objects.get(pk=1)
-#         with transaction.atomic():
-#             if username is None:
-#                 booking = models.BookedUsers.objects.create(user=user, username=None, id_proof=validated_data['id_proof'],
-#                                                   booked_date=timezone.now().date())
-#             else:
-#                 booking = models.BookedUsers.objects.create(user=None, username=username,
-#                                                   id_proof=validated_data['id_proof'],
-#                                                   booked_date=timezone.now().date())
-#             for i in validated_data['rooms']:
-#                 models.Booking.objects.create(room=i, check_in=validated_data['from_date'], check_out=validated_data['to_date'],
-#                                               status=0, reference=reference, booked_by=booking)
-#         return booking
-#
-#     def update(self, instance, validated_data):
-#         pass
-#
-#
-#     class Meta:
-#         fields = ('from_date', 'to_date', 'rooms', 'capacity', 'room_type', 'id_proof', 'username')
